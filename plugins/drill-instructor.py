59da2bf583d17bc34b4071b5e944373749d61630
Tom Barron <tbarron@ornl.gov>
2013-09-09 08:52:52 -0400

    Implementing plugins for demo - Add selected plugin source files to git - Support "fire = yes/no" option - Label log messages more clearly - Add '' as a key in map_time_unit so seconds is the default

---
+++ plugins/drill-instructor.py
@@ -0,0 +1,40 @@
+import ConfigParser
+import os
+import pexpect
+import sys
+import time
+
+def main(cfg):
+    clog = sys.modules['__main__'].get_logger()
+    clog.info("drill-instructor: FIRING")
+    clog.info("drill-instructor: cwd = %s" % os.getcwd())
+    hsi_prompt = "]:"
+
+    dicfg = ConfigParser.ConfigParser()
+    dicfg.read('plugins/drill-instructor.cfg')
+
+    # dump drill-instructor config to log
+    for s in dicfg.sections():
+        clog.info("drill-instructor: [%s]" % s)
+        for o in dicfg.options(s):
+            clog.info("drill-instructor: %s = %s" % (o, dicfg.get(s, o)))
+
+    
+    S = pexpect.spawn("/opt/public/bin/hsi")
+    S.logfile = f = open("hsi.out", 'a')
+
+    for s in dicfg.sections():
+        S.expect(hsi_prompt)
+        S.sendline("ls -X %s" % s)
+
+        dicfg.set(s, 'last-check', time.strftime("%Y.%m%d %H:%M:%S"))
+
+    S.expect(hsi_prompt)
+    S.sendline("quit")
+    S.expect(pexpect.EOF)
+
+    dicfg.write(open('plugins/drill-instructor.cfg', 'w'))
+    
+    S.logfile.close()
+    S.close()
+    
