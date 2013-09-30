c3ca8a344230f5e69a227d4cc9c627e1e2ebf985
Tom Barron <tbarron@ornl.gov>
2013-09-30 17:22:16 -0400

    NoOptionError, testing, fire plugins by default
     - Elevate ConfigParser.NoOptionError up into CrawlConfig
     - Get test_init_fire_* tests working
     - Make fire default to True in CrawlPlugin
    
    Change-Id: Iadf15049d3975d4899a7d8f4b4cb6897523b2844

--- CrawlPlugin.py
+++ CrawlPlugin.py
@@ -13,12 +13,15 @@ CrawlPlugin.py - Plugin class for HPSS integrity crawler
     will stay on the same schedule.
 
 """
-import ConfigParser
+import CrawlConfig
 import os
+import shutil
 import sys
+import testhelp
 import time
 import toolframe
 import unittest
+import util
 
 # -----------------------------------------------------------------------------
 class CrawlPlugin(object):
@@ -27,7 +30,8 @@ class CrawlPlugin(object):
         """
         Initialize this object.
         """
-        self.init_cfg_data(cfg, name)
+        self.cfg = cfg
+        self.init_cfg_data(name)
         self.last_fired = time.time() - self.frequency - 1
         super(CrawlPlugin, self).__init__()
 
@@ -40,16 +44,22 @@ class CrawlPlugin(object):
         self.last_fired = time.time()
         
     # -------------------------------------------------------------------------
-    def init_cfg_data(self, cfg, name=''):
+    def init_cfg_data(self, name=''):
         """
         Read data we care about from the configuration.
         """
-        self.cfg = cfg
         if name != '':
             self.name = name
-        self.firable = cfg.getboolean(self.name, 'fire')
-        self.frequency = cfg.get_time(self.name, 'frequency', 3600)
-        self.plugin_dir = cfg.get('crawler', 'plugin-dir')
+        try:
+            x = self.cfg.get(self.name, 'fire')
+            if x.lower() in self.cfg._boolean_states.keys():
+                self.firable = self.cfg._boolean_states[x]
+            else:
+                self.firable = False
+        except CrawlConfig.NoOptionError:
+            self.firable = True
+        self.frequency = self.cfg.get_time(self.name, 'frequency', 3600)
+        self.plugin_dir = self.cfg.get('crawler', 'plugin-dir')
         if self.plugin_dir not in sys.path:
             sys.path.append(self.plugin_dir)
         if self.name not in sys.modules.keys():
@@ -79,26 +89,188 @@ def CrawlPlugin_setup():
 
 # -----------------------------------------------------------------------------
 def CrawlPlugin_teardown():
-    pass
+    shutil.rmtree('test_plugins')
 
 # -----------------------------------------------------------------------------
 class CrawlPluginTest(unittest.TestCase):
-    # -------------------------------------------------------------------------
-    def test_init(self):
-        pass
-
-    # -------------------------------------------------------------------------
-    def test_fire(self):
-        pass
-
-    # -------------------------------------------------------------------------
-    def test_reload(self):
-        pass
+    plugdir = 'test_plugins'
     
     # -------------------------------------------------------------------------
-    def test_time_to_fire(self):
-        pass
+    def test_fire(self):
+        """
+        The plugin should fire and self.last_fired should be set
+        """
+        raise testhelp.UnderConstructionError()
 
+    # -------------------------------------------------------------------------
+    def test_init_fire_false(self):
+        """
+        If option 'fire' is False in config, should be False in plugin
+        """
+        pname = util.my_name()
+        self.make_plugin(pname)
+        c = CrawlConfig.CrawlConfig()
+        c.add_section(pname)
+        c.add_section('crawler')
+        c.set(pname, 'frequency', '19')
+        c.set('crawler', 'plugin-dir', self.plugdir)
+        c.set(pname, 'fire', 'false')
+        p = CrawlPlugin(pname, c)
+            
+        self.assertEqual(p.firable, False)
+
+    # -------------------------------------------------------------------------
+    def test_init_fire_true(self):
+        """
+        if option 'fire' is True in config, should be True in plugin
+        """
+        pname = util.my_name()
+        self.make_plugin(pname)
+        c = CrawlConfig.CrawlConfig()
+        c.add_section(pname)
+        c.add_section('crawler')
+        c.set(pname, 'frequency', '19')
+        c.set('crawler', 'plugin-dir', self.plugdir)
+        c.set(pname, 'fire', 'true')
+        p = CrawlPlugin(pname, c)
+            
+        self.assertEqual(p.firable, True)
+        
+    # -------------------------------------------------------------------------
+    def test_init_fire_unset(self):
+        """
+        If option 'fire' is not set in config, should be true in plugin
+        """
+        pname = util.my_name()
+        self.make_plugin(pname)
+        c = CrawlConfig.CrawlConfig()
+        c.add_section(pname)
+        c.add_section('crawler')
+        c.set(pname, 'frequency', '19')
+        c.set('crawler', 'plugin-dir', self.plugdir)
+        p = CrawlPlugin(pname, c)
+            
+        self.assertEqual(p.firable, True)
+        
+    # -------------------------------------------------------------------------
+    def test_init_freq_set(self):
+        """
+        if frequency is set in config, plugin should match
+        """
+        raise testhelp.UnderConstructionError()
+
+    # -------------------------------------------------------------------------
+    def test_init_freq_unset(self):
+        """
+        if frequency not set in config, should be 3600 in plugin
+        """
+        raise testhelp.UnderConstructionError()
+    # -------------------------------------------------------------------------
+    def test_init_plugdir_inspath(self):
+        """
+        if plugin_dir set in config and in sys.path, sys.path should not change
+        """
+        raise testhelp.UnderConstructionError()
+    # -------------------------------------------------------------------------
+    def test_init_plugdir_ninspath(self):
+        """
+        if plugin_dir set in config and not in sys.path, should be added to sys.path
+        """
+        raise testhelp.UnderConstructionError()
+    # -------------------------------------------------------------------------
+    def test_init_plugdir_unset(self):
+        """
+        if plugin_dir not set in config, should throw exception
+        """
+        raise testhelp.UnderConstructionError()
+    # -------------------------------------------------------------------------
+    def test_init_plugin_inmod(self):
+        """
+        if plugin does exist and is in module list, should be reloaded
+        """
+        c = CrawlConfig.CrawlConfig()
+        c.set('test_init', 'frequency', '19')
+        c.set('crawler', 'plugin-dir', 'plugins')
+        try:
+            p = CrawlPlugin.CrawlPlugin('test_init', c)
+            import_failed = False
+        except ImportError:
+            import_failed = True
+            
+        self.assertEqual(p.firable, True)
+        self.assertEqual(p.frequency, 19)
+        self.assertEqual(import_failed, True)
+        
+        raise testhelp.UnderConstructionError()
+
+    # -------------------------------------------------------------------------
+    def test_init_plugin_ninmod(self):
+        """
+        if plugin does exists and not in module list, should be imported
+        """
+        raise testhelp.UnderConstructionError()
+    # -------------------------------------------------------------------------
+    def test_init_plugin_nosuch(self):
+        """
+        if plugin does not exist, should get ImportError
+        """
+        raise testhelp.UnderConstructionError()
+
+    # -------------------------------------------------------------------------
+    def test_reload_fire(self):
+        """
+        Update fire
+        """
+        raise testhelp.UnderConstructionError()
+    
+    # -------------------------------------------------------------------------
+    def test_reload_freq(self):
+        """
+        Update frequency
+        """
+        raise testhelp.UnderConstructionError()
+    
+    # -------------------------------------------------------------------------
+    def test_reload_plugdir(self):
+        """
+        Update plugdir
+        """
+        raise testhelp.UnderConstructionError()
+    
+    # -------------------------------------------------------------------------
+    def test_reload_plugdir(self):
+        """
+        Update plugin
+        """
+        raise testhelp.UnderConstructionError()
+    
+    # -------------------------------------------------------------------------
+    def test_time_to_fire_false(self):
+        """
+        time_to_fire() should return False if time.time() - last_fired <= freq
+        """
+        raise testhelp.UnderConstructionError()
+
+    # -------------------------------------------------------------------------
+    def test_time_to_fire_true(self):
+        """
+        time_to_fire() should return True if freq < time.time() - last_fired
+        """
+        raise testhelp.UnderConstructionError()
+
+    # -------------------------------------------------------------------------
+    def make_plugin(self, pname):
+        if not os.path.isdir(self.plugdir):
+            os.mkdir(self.plugdir)
+        if not pname.endswith('.py'):
+            pname = pname + '.py'
+        f = open('%s/%s' % (self.plugdir, pname), 'w')
+        f.write("#!/bin/env python\n")
+        f.write("def main():\n")
+        f.write("    pass\n")
+        
+        f.close()
+        
 # -----------------------------------------------------------------------------
 if __name__ == '__main__':
     toolframe.ez_launch(setup=CrawlPlugin_setup,
