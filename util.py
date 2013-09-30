c810105062832b9d2289feca1cd25562dd56572a
Tom Barron <tbarron@ornl.gov>
2013-09-30 17:55:47 -0400

    Adding tests to CrawlPlugin
    Change-Id: I41ef59469e89dbe2d0c55074b67cdef811fcf76a

--- util.py
+++ util.py
@@ -1,5 +1,16 @@
 import sys
 
+# -----------------------------------------------------------------------------
+def contents(filename):
+    """
+    Return the contents of the file as a string
+    """
+    f = open(filename, 'r')
+    rval = f.read()
+    f.close()
+    return rval
+
+# -----------------------------------------------------------------------------
 def my_name():
     """
     Return the caller's name
