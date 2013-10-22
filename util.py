364605ac906ed014a623c94d7a7d2a56389915a4
Tom Barron <tbarron@ornl.gov>
2013-10-22 12:38:23 -0400

    Testing, python
     - Organize the tests a little better, so they call all be run with
       one command ("all_tests")
     - Added tests for util.py
     - Make sure everyone uses the same python
    
    Change-Id: I1109408276cc6818c999e5e43ac0f756a33f0980

--- util.py
+++ util.py
@@ -1,4 +1,9 @@
+#!/usr/bin/env python
+
+import pdb
 import sys
+import testhelp
+import unittest
 
 # -----------------------------------------------------------------------------
 def contents(filename):
@@ -16,3 +21,30 @@ def my_name():
     Return the caller's name
     """
     return sys._getframe(1).f_code.co_name
+
+# -----------------------------------------------------------------------------
+class UtilTest(unittest.TestCase):
+    # -------------------------------------------------------------------------
+    def test_content(self):
+        x = contents('./util.py')
+        self.assertEqual(type(x), str,
+                         "Expected a string but got a %s" % type(x))
+        expected = 'def contents('
+        self.assertIn(expected, x,
+                      "Expected to find '%s' in \"\"\"\n%s\n\"\"\"" %
+                      (expected, x))
+
+    # -------------------------------------------------------------------------
+    def test_my_name(self):
+        actual = my_name()
+        expected = 'test_my_name'
+        self.assertEqual(expected, actual,
+                         "Expected '%s' to match '%s'" %
+                         (expected, actual))
+
+# -----------------------------------------------------------------------------
+if __name__ == '__main__':
+    if '-d' in sys.argv:
+        sys.argv.remove('-d')
+        pdb.set_trace()
+    testhelp.main(sys.argv)
