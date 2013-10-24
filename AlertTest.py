b470fb53e5ae0dd1f141bd8129c20ef495a69893
Tom Barron <tbarron@ornl.gov>
2013-10-24 16:53:30 -0400

    Working on Alert class and tests for it

---
+++ AlertTest.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+import testhelp
+import toolframe
+
+def setUpModule():
+    pass
+
+def tearDownModule():
+    pass
+
+class AlertTest(testhelp.HelpedTestCase):
+    def test_
+
+if __name__ == '__main__':
+    toolframe.ez_launch(test='AlertTest',
+                        logfile='crawl_test.log')
