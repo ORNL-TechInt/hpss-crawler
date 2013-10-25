21b727fa48160321ce5c2d40ca270c351bc00afa
Tom Barron <tbarron@ornl.gov>
2013-10-25 10:02:21 -0400

    - Create symlink crawl -> crawl.py if it doesn't exist when starting    up tests.

--- all_tests.py
+++ all_tests.py
@@ -3,18 +3,23 @@ import os
 import pexpect
 import pdb
 import sys
+import toolframe
 import testhelp
 
-logfile = 'crawl_test.log'
-for modname in ['crawl',
-               'Checkable',
-               'CrawlConfig',
-               'CrawlPlugin',
-               'testhelp',
-               'util',
-              ]:
-    script = modname + '.py'
-    mod = __import__(modname)
-    tlist = testhelp.all_tests(modname)
-    print script + ":"
-    testhelp.run_tests([0], '', tlist, 1, logfile, mod)
+def main(args):
+    logfile = 'crawl_test.log'
+    for modname in ['crawl',
+                    'Checkable',
+                    'CrawlConfig',
+                    'CrawlPlugin',
+                    'testhelp',
+                    'util',
+                    ]:
+        script = modname + '.py'
+        mod = __import__(modname)
+        tlist = testhelp.all_tests(modname)
+        print script + ":"
+        testhelp.run_tests([0], '', tlist, 1, logfile, mod)
+
+if __name__ == '__main__':
+    toolframe.ez_launch(main=main)
