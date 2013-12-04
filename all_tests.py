7aba67ae3b8f1a9c53a434c667624b21e820e611
Tom Barron <tbarron@ornl.gov>
2013-12-04 12:01:44 -0500

    Dimension, testing
     - Removed db_initialized flag from Dimension.py
     - Added Dimension to all_tests
    
    Change-Id: Ia9a21f55f115e5a3cc08f4ab6da1078161c4cba1

--- all_tests.py
+++ all_tests.py
@@ -17,6 +17,7 @@ def main(args):
                     'Checkable',
                     'CrawlConfig',
                     'CrawlPlugin',
+                    'Dimension',
                     'testhelp',
                     'util',
                     ]:
