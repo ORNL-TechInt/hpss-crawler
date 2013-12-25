a91876f7aeefcce2793b210134b6c9d34fad3d8c
Tom Barron <tbarron@ornl.gov>
2013-12-25 12:54:40 -0500

    Created tests/hpssTest.py to test code in hpss.py

--- hpss.py
+++ hpss.py
@@ -2,26 +2,50 @@ import pexpect
 import sys
 
 # -----------------------------------------------------------------------------
+class HSIerror(Exception):
+    """
+    This class is used to return HSI errors to the application
+    """
+    def __init__(self, value):
+        """
+        Set the value for the exception. It should be a string.
+        """
+        self.value = str(value)
+    def __str__(self):
+        """
+        Report the exception value (should be a string).
+        """
+        return "%s" % (str(self.value))
+    
+# -----------------------------------------------------------------------------
 class HSI(object):
     # -------------------------------------------------------------------------
-    def __init__(self, *args, **kwargs):
+    def __init__(self, connect=True, *args, **kwargs):
         self.prompt = "]:"
         self.verbose = False
+        self.unavailable = False
         
         cmdopts = " ".join(args)
         for key in kwargs:
             setattr(self, key, kwargs[key])
 
-        cmd = "hsi " + cmdopts
-        if hasattr(self, 'timeout'):
-            self.xobj = pexpect.spawn(cmd, timeout=self.timeout)
-        else:
-            self.xobj = pexpect.spawn(cmd)
+        self.cmd = "hsi " + cmdopts
+        if connect:
+            self.connect()
 
+    # -------------------------------------------------------------------------
+    def connect(self):
+        if hasattr(self, 'timeout'):
+            self.xobj = pexpect.spawn(self.cmd, timeout=self.timeout)
+        else:
+            self.xobj = pexpect.spawn(self.cmd)
         if self.verbose:
             self.xobj.logfile = sys.stdout
-            
-        self.xobj.expect(self.prompt)
+        which = self.xobj.expect([self.prompt,
+                                  "HPSS Unavailable",
+                                  "connect: Connection refused"])
+        if 0 != which or self.unavailable:
+            raise HSIerror("HPSS Unavailable")
         
     # -------------------------------------------------------------------------
     def lscos(self):
