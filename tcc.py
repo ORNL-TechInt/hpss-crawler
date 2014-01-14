0f6f01f8bb427b574841b59533ce80a667ff3d71
Tom Barron <tbarron@ornl.gov>
2014-01-14 12:46:46 -0500

    Tape Copy Checker: ability to connect to DB2 remotely

--- tcc.py
+++ tcc.py
@@ -15,6 +15,137 @@ import time
 import toolframe
 
 # -----------------------------------------------------------------------------
+def tcc_remote(args):
+    """remote - test connecting to a remote database
+
+    usage: tcc remote -C <config-file>
+                      -c <config-section>
+                      -h <hostname>
+                      -D <database>
+                      -p <port>
+                      -u <username>
+                      -P <password>
+                      -s <optional sql statement>
+    """
+    p = optparse.OptionParser()
+
+    p.add_option('-d', '--debug',
+                 action='store_true', default=False, dest='debug',
+                 help='run the debugger')
+    p.add_option('-s', '--sql',
+                 action='store', default='', dest='sql',
+                 help='optional sql statement')
+    p.add_option('-D', '--database',
+                 action='store', dest='database',
+                 help='name of database to connect')
+
+    cg = optparse.OptionGroup(p, "config",
+                              "-C and -c take the connection info from "
+                              "a configuration file. They are not compatible "
+                              "with the cmdline options")
+    cg.add_option('-C', '--configfile',
+                 action='store', dest='cfgfile',
+                 help='which config file to use')
+    cg.add_option('-c', '--config',
+                 action='store', dest='cfgsect',
+                 help='which config file section to use')
+    p.add_option_group(cg)
+
+    cl = optparse.OptionGroup(p, "cmdline",
+                              "These options allow you to specify the "
+                              "database connection information on the "
+                              "command line. They are not compatible with "
+                              "the config options")
+    cl.add_option('-H', '--hostname',
+                 action='store', dest='hostname',
+                 help='name of host to connect')
+    cl.add_option('-p', '--port',
+                 action='store', dest='port',
+                 help='TCP port num to use')
+    cl.add_option('-u', '--username',
+                 action='store', dest='username',
+                 help='DB2 username')
+    cl.add_option('-P', '--password',
+                 action='store', dest='password',
+                 help='DB2 password')
+    p.add_option_group(cl)
+    
+    (o, a) = p.parse_args(args)
+
+    if o.debug: pdb.set_trace()
+
+    # at this point, all the args in one group or the other should be set. To
+    # check this, I need a count of the options set in each group. One should
+    # be 0, the other should be non-zero, and I need to be able to tell which
+    # group has the non-zero count.
+
+    # get a list of the attributes of o that represent specified arguments
+    s = [x for x in dir(o) if getattr(o, x) is not None and
+         not callable(getattr(o, x)) and
+         not x.startswith('_')]
+
+    # the following gives us a list of lists of n occurrences of the group
+    # title where n is the number of options from that group that were actually
+    # specified on the command line. The number of elements in this list of
+    # lists should be 1 since only options from one group or the other should
+    # be specified.
+    gl = [l for l in [[g.title for x in s if x in
+                         [n.dest for n in g.option_list]]
+                        for g in p.option_groups] if l != []]
+    if 1 < len(gl):
+        p.print_help()
+        p.error("Options from the config and cmdline groups "
+                "are not compatible")
+
+    # Now throw away the empty list from gl and let's look at what we have left
+    [sl] = [x for x in gl if x != []]
+
+    # the length of sl should match the list of option_list from the option group
+    [og] = [g for g in p.option_groups if g.title == sl[0]]
+    if len(og.option_list) != len(sl):
+        p.print_help()
+        p.error("Whichever option group is used, all options in it "
+                "must be specified")
+
+    if o.hostname is not None:
+        hostname = o.hostname
+        port = o.port
+        username = o.username
+        password = o.password
+    elif o.cfgfile is not None:
+        cfg = CrawlConfig.get_config(o.cfgfile)
+        hostname = cfg.get(o.cfgsect, 'hostname')
+        port = cfg.get(o.cfgsect, 'port')
+        username = cfg.get(o.cfgsect, 'username')
+        password = base64.b64decode(cfg.get(o.cfgsect, 'password'))
+        
+    db = db2.connect("database=%s;" % o.database +
+                     "hostname=%s;" % hostname +
+                     "port=%s;" % port +
+                     "uid=%s;" % username +
+                     "pwd=%s" % password,
+                     "",
+                     "")
+    if o.sql == 'cbf':
+        result = copies_by_file(db, o.database)
+        print "got %d rows" % len(result)
+        for row in result:
+            print row
+    elif o.sql != '':
+        r = db2.exec_immediate(db, o.sql)
+        x = db2.fetch_assoc(r)
+        while x:
+            pprint.pprint(x)
+            x = db2.fetch_assoc(r)
+    else:
+        r = db2.tables(db, 'SYSCAT', '%')
+        x = db2.fetch_assoc(r)
+        while x:
+            pprint.pprint(x)
+            x = db2.fetch_assoc(r)
+    db2.close(db)
+
+# -----------------------------------------------------------------------------
 def tcc_bfid(args):
     """bfid - report  a list of bfids
 
@@ -24,11 +155,15 @@ def tcc_bfid(args):
     p.add_option('-d', '--debug',
                  action='store_true', default=False, dest='debug',
                  help='run the debugger')
+    p.add_option('-D', '--db',
+                 action='store', default='', dest='dbsect',
+                 help='which database to access')
     (o, a) = p.parse_args(args)
 
     if o.debug: pdb.set_trace()
     
-    for row in query("select bfid, bfattr_cos_id, bfattr_create_time from bitfile"):
+    for row in query("select bfid, bfattr_cos_id, bfattr_create_time from bitfile",
+                     dbsect=o.dbsect):
         ct = time.strftime("%Y.%m%d %H:%M:%S",
                            time.localtime(row['BFATTR_CREATE_TIME']))
         print "%s %d %s" % (hexstr(row['BFID']),
@@ -45,11 +180,15 @@ def tcc_bfts(args):
     p.add_option('-d', '--debug',
                  action='store_true', default=False, dest='debug',
                  help='run the debugger')
+    p.add_option('-D', '--db',
+                 action='store', default='', dest='dbsect',
+                 help='which database to access')
     (o, a) = p.parse_args(args)
 
     if o.debug: pdb.set_trace()
     
-    for row in query("select bfid, storage_class from bftapeseg"):
+    for row in query("select bfid, storage_class from bftapeseg",
+                     dbsect=o.dbsect):
         print("%s %s" % (hexstr(row['BFID']), row['STORAGEE_CLASS']))
         
 # -----------------------------------------------------------------------------
@@ -87,13 +226,37 @@ def tcc_copies_by_file(args):
     p.add_option('-d', '--debug',
                  action='store_true', default=False, dest='debug',
                  help='run the debugger')
+    p.add_option('-D', '--db',
+                 action='store', default='', dest='dbsect',
+                 help='which database to access')
     (o, a) = p.parse_args(args)
 
     if o.debug: pdb.set_trace()
     
-    result = copies_by_file()
+    result = copies_by_file(dbsect=o.dbsect)
     for row in result:
         print row
+
+# -----------------------------------------------------------------------------
+def tcc_dblist(args):
+    """dblist - display a list of accessible databases
+
+    usage: tcc dblist
+    """
+    p = optparse.OptionParser()
+    p.add_option('-d', '--debug',
+                 action='store_true', default=False, dest='debug',
+                 help='run the debugger')
+    p.add_option('-D', '--db',
+                 action='store', default='', dest='dbsect',
+                 help='which database to access')
+    (o, a) = p.parse_args(args)
+
+    if o.debug: pdb.set_trace()
+    
+    cfg = CrawlConfig.get_config("tcc.cfg")
+    for s in cfg.sections():
+        print("   %s" % s)
 
 # -----------------------------------------------------------------------------
 def tcc_report(args):
@@ -109,12 +272,15 @@ def tcc_report(args):
     p.add_option('-d', '--debug',
                  action='store_true', default=False, dest='debug',
                  help='run the debugger')
+    p.add_option('-D', '--db',
+                 action='store', default='', dest='dbsect',
+                 help='which database to access')
     (o, a) = p.parse_args(args)
 
     if o.debug: pdb.set_trace()
     
     cbc = copies_by_cos()
-    cbf = copies_by_file()
+    cbf = copies_by_file(dbsect=o.dbsect)
     print("%63s %10d %6s %5d" % ("Bitfile ID",
                                  "File Count",
                                  "COS ID",
@@ -136,12 +302,15 @@ def tcc_selbf(args):
     p.add_option('-d', '--debug',
                  action='store_true', default=False, dest='debug',
                  help='run the debugger')
+    p.add_option('-D', '--db',
+                 action='store', default='', dest='dbsect',
+                 help='which database to access')
     (o, a) = p.parse_args(args)
 
     if o.debug: pdb.set_trace()
     
     record = 0
-    for row in query("select * from bitfile"):
+    for row in query("select * from bitfile", dbsect=o.dbsect):
         print("--- record %d ---" % record)
         record += 1
         for k in sorted(row):
@@ -167,8 +336,8 @@ def tcc_sql(args):
     p.add_option('-d', '--debug',
                  action='store_true', default=False, dest='debug',
                  help='run the debugger')
-    p.add_option('-D', '--dbname',
-                 action='store', default='', dest='dbname',
+    p.add_option('-D', '--db',
+                 action='store', default='', dest='dbsect',
                  help='use an alternate database')
     (o, a) = p.parse_args(args)
 
@@ -176,7 +345,7 @@ def tcc_sql(args):
     
     sql = " ".join(a)
     print sql
-    for row in query(sql, dbname=o.dbname):
+    for row in query(sql, dbsect=o.dbsect):
         pprint.pprint(row)
 
 # -----------------------------------------------------------------------------
@@ -215,7 +384,7 @@ def copies_by_cos():
     return cbc
 
 # -----------------------------------------------------------------------------
-def copies_by_file():
+def copies_by_file(dbc=None, dbsect=''):
     """
     This does a left outer join between tables bitfile and bftapeseg. Depending
     on the quantity of data in those tables, this request may take a while to
@@ -226,7 +395,8 @@ def copies_by_file():
                    "from hpss.bitfile A left outer join hpss.bftapeseg B " +
                    "on A.bfid = B.bfid " +
                    "where A.bfattr_data_len > 0 and B.bf_offset = 0 " +
-                   "group by A.bfid, A.bfattr_cos_id, A.bfattr_create_time")
+                   "group by A.bfid, A.bfattr_cos_id, A.bfattr_create_time",
+                   dbsect=dbsect)
     return result
 
 # -----------------------------------------------------------------------------
@@ -278,15 +448,33 @@ def hpss_userpass():
     return (username, password)
 
 # -----------------------------------------------------------------------------
-def query(sql, dbname=''):
+def query(sql, dbsect=''):
     """
     Connect to a DB2 database, run an sql command (assumed to be a select), and
     return the result.
     """
-    if dbname == '':
-        dbname = 'SUBSYS'
-    (username, password) = hpss_userpass()
-    db = db2.connect(dbname, username, password)
+    if dbsect == '':
+        dbsect = 'dev1-subsys'
+
+    cfg = CrawlConfig.get_config('tcc.cfg')
+    username = cfg.get(dbsect, 'username')
+    password = base64.b64decode(cfg.get(dbsect, 'password'))
+    if username == 'retrieve':
+        (username, password) = hpss_userpass()
+    dbname = cfg.get(dbsect, 'dbname')
+    dbargs = [dbname, username, password]
+    if cfg.has_option(dbsect, 'hostname'):
+        hostname = cfg.get(dbsect, 'hostname')
+        port = cfg.get(dbsect, 'port')
+        dbargs = ["database=%s;" % dbname +
+                  "hostname=%s;" % hostname +
+                  "port=%s;" % port +
+                  "uid=%s;" % username +
+                  "pwd=%s;" % password,
+                  "",
+                  ""]
+    db = db2.connect(*dbargs)
+    
     r = db2.exec_immediate(db, sql)
     rval = []
     x = db2.fetch_assoc(r)
