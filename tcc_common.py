c714d71edca7feb365f11a424b1791c6e1b8ac0f
Tom Barron <tbarron@ornl.gov>
2014-02-12 09:06:29 -0500

    Fixes from production to get tape_copy_checker working (mostly)

--- tcc_common.py
+++ tcc_common.py
@@ -3,9 +3,15 @@ def hexstr(bfid):
     """
     Convert a raw bitfile id into a hexadecimal string as presented by DB2.
     """
-    rval = "x'"
-    for c in list(bfid):
-        rval += "%02x" % ord(c)
-    rval += "'"
+    rval = "x'" + hexstr_uq(bfid) + "'"
     return rval
+
+# -----------------------------------------------------------------------------
+def hexstr_uq(bfid):
+    """
+    Convert a raw bitfile id into an unquoted hexadecimal string as presented
+    by DB2.
+    """
+    rval = "".join(["%02x" % ord(c) for c in list(bfid)])
+    return rval.upper()
 
