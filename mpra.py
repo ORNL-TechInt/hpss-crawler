e86ba8f3535b986f7d9ccb26562a3ca901046fff
Tom Barron <tbarron@ornl.gov>
2014-03-07 10:18:05 -0500

    More functionality

--- mpra.py
+++ mpra.py
@@ -8,8 +8,9 @@ import util
 def mpra_migr_recs():
     """migr_recs - list the records in table BFMIGRREC
 
-    usage: mpra migr_recs [-l N] [-b/--before DATE-TIME]
-                                 [-a/--after DATE-TIME]
+    usage: mpra migr_recs [-l/limit N]
+                          [-b/--before DATE-TIME]
+                          [-a/--after DATE-TIME]
                                  
     with -l N, only report the first N records
 
@@ -23,9 +24,15 @@ def mpra_migr_recs():
     p.add_option('-d', '--debug',
                  action='store_true', default=False, dest='debug',
                  help='run the debugger')
-    p.add_option('-D', '--db',
-                 action='store', default='', dest='dbsect',
-                 help='which database to access')
+    p.add_option('-l', '--limit',
+                 action='store', default='', dest='limit',
+                 help='how many records to fetch')
+    p.add_option('-b', '--before',
+                 action='store', default='', dest='before',
+                 help='fetch records from before the date/time')
+    p.add_option('-a', '--after',
+                 action='store', default='', dest='after',
+                 help='fetch records from after the date/time')
     (o, a) = p.parse_args(args)
 
     if o.debug: pdb.set_trace()
@@ -36,8 +43,18 @@ def mpra_migr_recs():
         dbname = 'hsubsys1'
         
     db = CrawlDBI.DBI('db2', dbname=dbname)
-    rows = db.select(table='bfmigrrec')
-              
+    if o.limit != '':
+        rows = db.select(table='bfmigrrec', limit=int(o.limit))
+    elif o.before != '':
+        when = time.mktime(time.strptime(o.before, "%Y.%m%d %H:%M:%S"))
+        rows = db.select(table='bfmigrrec',
+                         where="record_create_time < ?",
+                         data=(o.before,))
+    elif o.after != '':
+        when = time.mktime(time.strptime(o.after, "%Y.%m%d %H:%M:%S"))
+        rows = db.select(table='bfmigrrec',
+                         where="record_create_time > ?",
+                         data=(o.before,))
 # -----------------------------------------------------------------------------
 def mpra_purge_recs():
     """purge_recs - list the records in table BFPURGEREC
