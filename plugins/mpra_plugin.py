864cebd5bb6b6a6cf3cbbeaeae1c577f93d1a33c
Tom Barron <tbarron@ornl.gov>
2014-03-31 16:50:18 -0400

    CrawlConfig.py, CrawlPlugin, mpra:
     - Add to_seconds() to externalize support for converting time specs
       into a number of seconds.
    
     - Use self.modname (not sys.name) as key into sys.modules.
    
     - Add --start, --end to mpra_age so we can use raw epoch values.
     - Add mpra_date_age to convert a YYYY.mmdd date into an age.
     - Add mpra_epoch to convert an epoch value to YYYY.mmdd format.
    
     - Report record ages in mpra report in human friendly terms.
     - Only update the recent value if it has gotten bigger.
    
     - Use CrawlConfig.get_time() rather than mpra_lib.age_seconds().
     - Log the number of new records found at each iteration.
    
    Change-Id: I10e0f1385cf8928a19c110027ff859fc4b6d9753

--- plugins/mpra_plugin.py
+++ plugins/mpra_plugin.py
@@ -13,19 +13,21 @@ def main(cfg):
     """
     if cfg is None:
         cfg = CrawlConfig.get_config()
-    age = cfg.get('mpra', 'age')
+    age = cfg.get_time('mpra', 'age')
 
-    end = time.time() - mpra_lib.age_seconds(age)
+    end = time.time() - age
 
     start = mpra_lib.mpra_fetch_recent("migr")
     util.log("searching for migration records betweeen %d and %d" %
              (start, end))
     result = mpra_lib.age("migr", start=start, end=end, mark=True)
-                
+    util.log("found %d migration records in the range" % result)
+    
     start = mpra_lib.mpra_fetch_recent("purge")
     util.log("searching for purge records betweeen %d and %d" %
              (start, end))
     result = mpra_lib.age("purge", start=start, end=end, mark=True)
+    util.log("found %d purge records in the range" % result)
                 
             
         
