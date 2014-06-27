989769eaa269872434186d9a54828f18826be2a8
Tom Barron <tbarron@ornl.gov>
2014-06-27 11:29:58 -0400

    Be careful not to dump defaults when they are not requested in CrawlConfig
    Change-Id: I669f5b7db68782a718b917ebf51ac100072629d7

--- test/test_toolframe.py
+++
@@ -1,8 +0,0 @@
-# - sitting a dir A, creating symlink in A/B for A/B/foo.py, symlink should be
-#   A/B/foo -> foo.py, not A/B/foo -> B/foo.py
-
-# running 'foo.py' should run the tests, whether or not the symlink exists.
-# both ez_launch() and tf_launch() need a test for this
-
-# running 'foo.py -L' should create the symlink. Both ez_launch() and
-# tf_launch() need a test for this
