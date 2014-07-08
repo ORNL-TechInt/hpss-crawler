e8e1e2594043cc68d9c66002a7a4c21543efbe13
Tom Barron <tbarron@ornl.gov>
2014-07-08 12:57:30 -0400

    - In DB2 select, support joins with tests. - Finish eliminating ibm_db direct dependency from tcc, mpra code
    Change-Id: Id9f59ee3466f44094f50e0dc334bcdb6c363ce70

--- hpssic/test/test_CrawlDBI.py
+++ hpssic/test/test_CrawlDBI.py
@@ -1673,6 +1673,30 @@ class DBIdb2Test(DBI_in_Base, DBITestRoot):
                         "Expected at least one row, got 0")
 
     # -------------------------------------------------------------------------
+    def test_select_join(self):
+        """
+        Select should support joining tables.
+        """
+        db = CrawlDBI.DBI(cfg=make_tcfg(self.dbtype))
+        rows = db.select(table=['server', 'logclient'],
+                         fields=['server_id', 'desc_name', 'logc_directory'],
+                         where="server_id = logc_server_id")
+        self.assertTrue(0 < len(rows),
+                        "Expected at least one row, got 0")
+
+    # -------------------------------------------------------------------------
+    def test_select_join_tn(self):
+        """
+        Select should support joining tables with temporary table names.
+        """
+        db = CrawlDBI.DBI(cfg=make_tcfg(self.dbtype))
+        rows = db.select(table=['server A', 'logclient B'],
+                         fields=['A.server_id', 'A.desc_name', 'B.logc_directory'],
+                         where="A.server_id = B.logc_server_id")
+        self.assertTrue(0 < len(rows),
+                        "Expected at least one row, got 0")
+
+    # -------------------------------------------------------------------------
     #   - limit not an int
     #     > should throw exception
     def test_select_l_nint(self):
