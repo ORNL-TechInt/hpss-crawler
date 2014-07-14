ce949c05c8a21471f0b592dfc407b5d8f0dfee95
Tom Barron <tbarron@ornl.gov>
2014-07-14 17:39:33 -0400

    What's in /opt?
    Change-Id: Ied7669bdc196a55c7c34130bcf46743d5dd36e4f

--- test.sh
+++ test.sh
@@ -12,8 +12,9 @@ export PATH=${PATH}:${LOCAL}/bin
 
 which python
 python --version
-env
-ls /usr/lib/python2.6/site-packages
+# env
+# ls /usr/lib/python2.6/site-packages
+ls /opt
 nosetests1.1 -c test/nosecron.cfg test
 
 #
