bf35c82e89400efe8daf489762cc94da3aa81c95
Tom Barron <tbarron@ornl.gov>
2014-07-15 11:53:18 -0400

    Determine $HOME and cwd
    Change-Id: If80dc5f3a7b2424bf146a42b8ea7d00861596af7

--- test.sh
+++ test.sh
@@ -1,4 +1,10 @@
-which easy_install
-
+echo $HOME
+pwd
+# easy_install --user virtualenv
+# export PATH=${PATH}:$HOME/.local/bin
+# virtualenv hpssic
+# export PATH=${PATH}:hpssic/bin
+# . hpssic/bin/activate
+# pip install pexpect
 # nosetests1.1 -c test/nosecron.cfg hpssic/test
 
