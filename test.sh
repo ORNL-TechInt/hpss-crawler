1b31ad1740dda17d00a1c3569523004bdf15acc2
Tom Barron <tbarron@ornl.gov>
2014-07-15 11:16:54 -0400

    Point nose at hpssic/test, not the dead symlink
    Change-Id: I3d1f07381915d5b23b963ad8521aa0fadba4afb7

--- test.sh
+++ test.sh
@@ -14,7 +14,7 @@ which python
 python --version
 which nosetests
 which nosetests1.1
-nosetests1.1 -c test/nosecron.cfg test
+nosetests1.1 -c test/nosecron.cfg hpssic/test
 
 #
 # Next, we set up a virtual environment where we can install all our
