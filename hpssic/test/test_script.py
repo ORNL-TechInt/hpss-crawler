469f3a816f5ca3b7324fa9512cab5b0f07206fa4
Tom Barron <tbarron@ornl.gov>
2014-08-31 10:16:18 -0400

    Finishing tests for dbtype/dbname rules
     - Complete test_ctor_dbtype_hpss_ tests (and moved them to the DBIdb2Test
       class)
     - Make test_pep8 ignore emacs' .#* work files
     - In assertRaisesMsg(), report expected and got aligned on two lines to make
       it easy to see the differences.
    
    Change-Id: Ie9dd0ada97e36f6213cc5da2213262026800b838

--- hpssic/test/test_script.py
+++ hpssic/test/test_script.py
@@ -315,9 +315,14 @@ class Test_MISC(th.HelpedTestCase):
     def test_pep8(self):
         result = ""
         for r, d, f in os.walk('hpssic'):
-            pylist = [os.path.join(r, fn) for fn in f if fn.endswith('.py')]
+            pylist = [os.path.join(r, fn)
+                      for fn in f
+                      if fn.endswith('.py') and not fn.startswith(".#")]
             inputs = " ".join(pylist)
-            if any([r == "./test", ".git" in r, ".attic" in r, "" == inputs]):
+            if any([r == "./test",
+                    ".git" in r,
+                    ".attic" in r,
+                    "" == inputs]):
                 continue
             result += pexpect.run("pep8 %s" % inputs)
         self.assertEqual("", result, "\n" + result)
