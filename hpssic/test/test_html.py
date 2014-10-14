25575b01fd329a408269887c58dff2dc2f104970
Tom Barron <tbarron@ornl.gov>
2014-10-14 10:32:46 -0400

    Add test for 'html report'
    Version:   2014.1009.7
    Change-Id: I0d3c53e3a81f88643174e68d53f604fdf68d43f0

--- hpssic/test/test_html.py
+++ hpssic/test/test_html.py
@@ -1,9 +1,14 @@
 """
 Tests for html.py
 """
+from hpssic import cv_sublib
 from hpssic import html
+from hpssic import mpra_sublib
+import pexpect
+from hpssic import tcc_sublib
 from hpssic import testhelp
 from hpssic import util as U
+
 
 # -----------------------------------------------------------------------------
 class HtmlTest(testhelp.HelpedTestCase):
@@ -15,4 +20,15 @@ class HtmlTest(testhelp.HelpedTestCase):
         Try running 'html report > filename' and verify that 1) no traceback
         occurs and 2) something is actually written to the output file.
         """
-        self.fail('construction')
+        result = pexpect.run("html report")
+        self.assertTrue(str == type(result),
+                        "Expected result of type str, got %s" % type(result))
+
+        self.assertTrue("Traceback" not in result,
+                        '"""%s"""should not contain "Traceback"' % result)
+
+        self.expected_in(cv_sublib.report_title(), result)
+        self.expected_in(mpra_sublib.report_title(), result)
+        self.expected_in(tcc_sublib.report_title(), result)
+
+        self.expected_in("cos  *Population  *Sample", result)
