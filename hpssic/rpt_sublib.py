61286eb389a9b0bb3c0114321611eac64135592b
Tom Barron <tbarron@ornl.gov>
2014-11-15 13:19:01 -0500

    Write function rpt_sublib.cos_description
     - This is to support the update to Dimension.report() to add COS
       descriptions to the CV report
    
    Version:   2014.1112.13
    Change-Id: I3ea02b7b475d07b3b1c18f8e46eb2f979b069485

--- hpssic/rpt_sublib.py
+++ hpssic/rpt_sublib.py
@@ -1,1 +1,20 @@
 import CrawlConfig
+import CrawlDBI
+import cv_sublib
+
+
+# -----------------------------------------------------------------------------
+def cos_description(name):
+    db = CrawlDBI.DBI(dbtype='crawler')
+    if not db.table_exists(table='lscos'):
+        cv_sublib.lscos_populate()
+
+    r = db.select(table='lscos',
+                  fields=['name'],
+                  where="name = ?",
+                  data=(name,))
+    if 0 < len(r):
+        rval = r[0][0]
+    else:
+        rval = "???"
+    db.close()
