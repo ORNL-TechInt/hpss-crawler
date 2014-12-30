d21b5a4ffaea0a950db492d83c6a3f2878073b4c
Tom Barron <tbarron@ornl.gov>
2014-12-30 16:06:41 -0500

    Allow for tests outside of a class
     - If cls is None, don't try to get a name from it
     - Set dbgfunc in pytest
    
    Version:   2014.1217.16
    Change-Id: I78842641b13ea11e66d56048722f194dbb1827cf

--- conftest.py
+++ conftest.py
@@ -53,12 +53,21 @@ def pytest_runtest_setup(item):
     """
     Decide whether to skip a test before running it
     """
-    fqn = '.'.join([item.module.__name__, item.cls.__name__, item.name])
+    if item.cls is None:
+        fqn = '.'.join([item.module.__name__, item.name])
+    else:
+        fqn = '.'.join([item.module.__name__, item.cls.__name__, item.name])
     dbg_n = '..' + item.name
     dbg_l = item.config.getvalue('dbg')
     skip_l = item.config.getvalue('skip')
     if dbg_n in dbg_l or '..all' in dbg_l:
         pdb.set_trace()
+
+    if any([item.name in item.config.getoption('--dbg'),
+            'all' in item.config.getoption('--dbg')]):
+        pytest.dbgfunc = pdb.set_trace
+    else:
+        pytest.dbgfunc = lambda: None
 
     jfm = 'jenkins_fail'
     if item.get_marker(jfm):
