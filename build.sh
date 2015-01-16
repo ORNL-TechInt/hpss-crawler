8e72275351066ca330bba77ae144328fbc15ba94
Tom Barron <tbarron@ornl.gov>
2015-01-16 10:08:33 -0500

    RPMS wind up in wrong yum repo
     - Fix: set environment variable REPO_DESTINATION to hpss-generic6-x86_64
    
    Version:   2015.0113.hf.4
    Change-Id: Ie4a038b5ced4986be8d9decfb985beaf84942cdc

--- build.sh
+++ build.sh
@@ -1,4 +1,9 @@
 python setup.py bdist_rpm
 mkdir RPMS
 cp dist/*.rpm RPMS
-
+ls -lR .
+# REPO_DESTINATION=jenkins-built-for-testing
+# REPO_DISTRO=generic6-x86_64
+# copied to /ccs/siterepos/$REPO_DESTINATION/$REP_DISTRO
+ls -alR /css/siterepos
+export REPO_DESTINATTION=hpss-generic6-x86_64
