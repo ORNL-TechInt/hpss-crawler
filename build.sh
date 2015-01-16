69533cf65e96976a3c60fbef7e6a230cd334db0a
Tom Barron <tbarron@ornl.gov>
2015-01-16 10:59:57 -0500

    RPMs not winding up where we want them
     - Fix: Use destinations.txt per Jason K's instruction
    
    Version:   2015.0113.hf.7
    Change-Id: I2c6d4f081de3aa0f6bb3dff92a99c45a97544316

--- build.sh
+++ build.sh
@@ -1,9 +1,4 @@
 python setup.py bdist_rpm
 mkdir RPMS
 cp dist/*.rpm RPMS
-ls -l ..
-# REPO_DESTINATION=jenkins-built-for-testing
-# REPO_DISTRO=generic6-x86_64
-# copied to /ccs/siterepos/$REPO_DESTINATION/$REP_DISTRO
-ls -al /ccs/siterepos/*
-export REPO_DESTINATTION=hpss-generic6-x86_64
+echo "hpssic-2015.0113.hf.6-1.noarch.rpm hpss generic6-x86_64" > destinations.txt
