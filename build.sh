92ea4310492d6eec02c71f5c6d1f78814e0aec54
Tom Barron <tbarron@ornl.gov>
2015-01-16 11:19:47 -0500

    'RPMS' at front of rpm filename making trouble
     - Fix: Remove line setting RPM_NAME with hardcoded directory prefix
    
    Version:   2015.0113.hf.9
    Change-Id: I9050e295b89d98d187fadc36d84b086d3e097cfd

--- build.sh
+++ build.sh
@@ -3,8 +3,8 @@ python setup.py bdist_rpm
 mkdir RPMS
 cp dist/*.rpm RPMS
 
-cd RPMS; export RPM_NAME=`ls *noarch.rpm | head -1`; cd ..
 export REPO_DIR=hpss
 export REPO_ARCH=generic6-x86_64
-export RPM_NAME=`ls RPMS/*noarch.rpm`
+cd RPMS; export RPM_NAME=`ls *noarch.rpm | head -1`; cd ..
+
 echo "$RPM_NAME $REPO_DIR $REPO_ARCH" > destinations.txt
