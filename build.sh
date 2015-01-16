279003c0a96b757e912ae833bd0939b37e127e60
Tom Barron <tbarron@ornl.gov>
2015-01-16 12:12:10 -0500

    Only want to deploy RPM for release
     - Fix: In build.sh, only create destination.txt if $GERRIT_REFNAME
       contains 'ref/tags/'
    
    Version:   2015.0113.hf.10
    Change-Id: I079dd4f07030e08abec2b747d8b0d688012a3df8

--- build.sh
+++ build.sh
@@ -7,4 +7,6 @@ export REPO_DIR=hpss
 export REPO_ARCH=generic6-x86_64
 cd RPMS; export RPM_NAME=`ls *noarch.rpm | head -1`; cd ..
 
-echo "$RPM_NAME $REPO_DIR $REPO_ARCH" > destinations.txt
+if [[ `expr "$GERRIT_REFNAME" : "refs/tags/"` != "0" ]]; then
+    echo "$RPM_NAME $REPO_DIR $REPO_ARCH" > destinations.txt
+fi
