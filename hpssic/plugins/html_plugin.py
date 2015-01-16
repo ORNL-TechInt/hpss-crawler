42be50011a86a98928830e7f4b87822ccf681aec
Tom Barron <tbarron@ornl.gov>
2015-01-16 16:36:25 -0500

    html_plugin calls get_html_report with cfg not filename
     - Add hpssic/plugins/html_plugin.py
     - Fix: Update get_html_report() to accept cfg object, filename, or
       nothing. If it gets nothing, it will use the default config.
    
    Version:   2015.0113.5
    Change-Id: I06574e174fb3ec59022828c81d37df32ee4dcdda

---
+++ hpssic/plugins/html_plugin.py
@@ -0,0 +1,22 @@
+from hpssic import CrawlConfig
+from hpssic import html_lib
+import os
+
+
+# -----------------------------------------------------------------------------
+def main(cfg):
+    """
+    Generate an html-formatted report and store it at the designated location
+    """
+    CrawlConfig.log("html_plugin starting")
+    fpath = cfg.get('html', 'output_path')
+    rpt = html_lib.get_html_report(cfg=cfg)
+
+    npath = fpath + '.new'
+    opath = fpath + '.old'
+    with open(npath, 'w') as out:
+        out.write(rpt)
+
+    os.rename(fpath, opath)
+    os.rename(npath, fpath)
+    CrawlConfig.log("html_plugin finished")
