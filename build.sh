2629bdbf11f54c477cf2b670e28ae9cf4b9be655
Tom Barron <tbarron@ornl.gov>
2015-01-16 10:31:20 -0500

    Not listing contents of /ccs/siterepos
     - Fix: Add '/*' to the path
    
    Version:   2015.0113.hf.6
    Change-Id: Ideb714127299678e4cf11a34f542cd67875dc51e

--- build.sh
+++ build.sh
@@ -1,9 +1,9 @@
 python setup.py bdist_rpm
 mkdir RPMS
 cp dist/*.rpm RPMS
-ls -lR .
+ls -l ..
 # REPO_DESTINATION=jenkins-built-for-testing
 # REPO_DISTRO=generic6-x86_64
 # copied to /ccs/siterepos/$REPO_DESTINATION/$REP_DISTRO
-ls -alR /ccs/siterepos
+ls -al /ccs/siterepos/*
 export REPO_DESTINATTION=hpss-generic6-x86_64
